703d75a9e041423ac4614efba362ef2513c63bc8
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-21 00:25:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,12 +59,6 @@ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
 
 	programmer_hash =
  		{
@@ -107,7 +101,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]= "GUI"
 end
 
 def adding_to_dennis
