052396238d1dc1c2fe098d393138c2d9ca7b9586
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-21 00:06:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,8 +68,8 @@ def adding_matz
 
 	programmer_hash =
  		{
-			:yukihiro_matsumoto=> {
-				:known_for => "Ruby"
+			:yukihiro_matsumoto => {
+				:known_for => "Ruby",
 				:languages => ["LISP", "C"]
 			},
       :grace_hopper => {
